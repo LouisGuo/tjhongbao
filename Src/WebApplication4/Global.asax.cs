c07e28d153850b5d02cb5104ae824df64d20dd23
LouisGuo <2213429531@qq.com>
2016-11-03 00:54:22 +0800

    init

---
+++ Src/WebApplication4/Global.asax.cs
@@ -0,0 +1,39 @@
+﻿using Senparc.Weixin.MP.Containers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace WebApplication4
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            RegisterSenparcWeixin();//注册Demo所用微信公众号的账号信息
+
+            Senparc.Weixin.Config.IsDebug = true;//这里设为Debug状态时，/App_Data/目录下会生成日志文件记录所有的API请求日志，正式发布版本建议关闭
+
+
+
+        }
+
+        /// <summary>
+        /// 注册Demo所用微信公众号的账号信息
+        /// </summary>
+        private void RegisterSenparcWeixin()
+        {
+            AccessTokenContainer.Register(
+                System.Configuration.ConfigurationManager.AppSettings["WeixinAppId"],
+                System.Configuration.ConfigurationManager.AppSettings["WeixinAppSecret"],
+                "【盛派网络小助手】公众号");
+        }
+    }
+}
